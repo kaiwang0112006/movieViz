362f802346a5b51d28d12505aace77712cbbdff1
kaiwang0112006 <kaiwang0112006@yahoo.com>
2017-06-30 15:13:15 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+#### 说明
+
+豆瓣电影评分与票房数据可视化，见此[链接](https://kaiwang0112006.github.io/movieViz/src/movieViz.html)。动态网页可以访问[link](http://tasteofdata.com/movie_visualization/douban_box)
---
+++ js/movievis.js
@@ -0,0 +1,31 @@
+﻿function moviedraw(st, ed){
+	d3.csv("data/topmovie.csv", function(error, data){
+		    var svg = dimple.newSvg("#chartContainer", 800, 600);
+
+		    var myChart = new dimple.chart(svg, data);
+		    //myChart.setBounds(75, 30, 480, 330)
+
+		    var x = myChart.addMeasureAxis("x", "box");
+		    x.fontSize = "auto";
+		    var y = myChart.addMeasureAxis("y", "rate");
+		    y.fontSize = "auto";
+		    myChart.addMeasureAxis("z", "size");
+
+		    var s = myChart.addSeries(['movie','year',"director"], dimple.plot.bubble);
+
+		    s.afterDraw = function (shp, d) {
+		        var shape = d3.select(shp);
+		        svg.append("text")
+		            .attr("x", parseFloat(shape.attr("cx")))
+		            .attr("y", parseFloat(shape.attr("cy")) + 4)
+		            .style("text-anchor", "middle")
+		            .style("font-size", "15px")
+		            .style("font-family", "sans-serif")
+		            .style("opacity", 0.7)
+		            .text(d.aggField[0]);
+		    };
+		    myChart.addLegend(180, 10, 360, 20, "right");
+		    myChart.draw();
+		    document.getElementById('loading').innerHTML=""
+	})
+}
